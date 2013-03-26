792c6851ac9d30e43d54e99ef07f133365997e00
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-03-26 14:10:08 +0900

    filter-aaaa.lua: PowerDNS recursor 3.4+ is required

--- recursor/filter-aaaa.lua
+++ recursor/filter-aaaa.lua
@@ -1,7 +1,11 @@
 -- PowerDNS recursor: Filter AAAA records
 -- Copyright (c) 2013 SATOH Fumiyasu @ OSS Technology Corp., Japan
+--               https://GitHub.com/fumiyas/
+--               https://twitter.com/satoh_fumiyasu
 --
 -- License GPLv3+: GNU GPL version 3 or later
+--
+-- PowerDNS recursor 3.4+ is required
 
 function postresolve(remoteip, domain, qtype, records, rcode)
   rcode_new = rcode
